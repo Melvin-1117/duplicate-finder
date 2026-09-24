1d4bb5d6c1a69f0916a4282e46bbcd3d002ea73d
Melvimn-1117 <antonymelvin81@gmail.com>
2026-09-24 23:36:04 +0530

    Basic stream pipeline to collect the files implemented

--- src/main/java/com/anton/dupfinder/FileScanner.java
+++ src/main/java/com/anton/dupfinder/FileScanner.java
@@ -1,8 +1,10 @@
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
-public List<Path> scanFiles(Path rootPath){   //access specifier + return type + method name + parameter with variable name inside paranthesis
-                                              // public + List<Path> will return containing path + name of the method + receives path input
-    Files.walk(rootPath);                     // this line is responsible for repeatedly iterating through the given directory
+import java.util.stream.Collectors;
+    public List<Path> scanFiles(Path rootPath){   //access specifier + return type + method name + parameter with variable name inside paranthesis
+        // public + List<Path> will return containing path + name of the method + receives path input
+       return Files.walk(rootPath).filter(path -> Files.isRegularFile(path))                     // Recursively traverses the directory , filters and keeps only regular files
+                .collect(Collectors.toList());                                      // collector build in stream method collects the filtered regular files into a list
 }
 
