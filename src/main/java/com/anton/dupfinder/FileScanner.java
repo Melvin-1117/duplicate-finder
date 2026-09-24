65ec0c3ef53f78b7f1d680b57bf5d5813afa920a
Melvimn-1117 <antonymelvin81@gmail.com>
2026-09-25 00:34:21 +0530

    Try with resource implemented

--- src/main/java/com/anton/dupfinder/FileScanner.java
+++ src/main/java/com/anton/dupfinder/FileScanner.java
@@ -2,9 +2,17 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
 import java.util.stream.Collectors;
-    public List<Path> scanFiles(Path rootPath){   //access specifier + return type + method name + parameter with variable name inside paranthesis
-        // public + List<Path> will return containing path + name of the method + receives path input
-       return Files.walk(rootPath).filter(path -> Files.isRegularFile(path))                     // Recursively traverses the directory , filters and keeps only regular files
-                .collect(Collectors.toList());                                      // collector build in stream method collects the filtered regular files into a list
+import java.io.IOException;
+import java.util.stream.Stream;
+
+public List<Path> scanFiles(Path rootPath) {   //access specifier + return type + method name + parameter with variable name inside paranthesis
+    // public + List<Path> will return containing path + name of the method + receives path input
+    try(Stream<Path> stream = Files.walk(rootPath)){
+        return stream.filter(path -> Files.isRegularFile(path))                     // Recursively traverses the directory , filters and keeps only regular files
+                .collect(Collectors.toList());                                      // collector is a build in stream method collects the filtered regular files into a list
+    } catch (IOException fileMissing) {                                                                 //catch handles the exception throws in the file.walk, for example if the file or directory is missing it returns a message.
+        System.out.println("Unable to scan directory : " + fileMissing.getMessage());
+        return List.of();
+    }
 }
 
